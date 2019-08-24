1c77b3164637f4f9d6fa08cd65d7ee4080c71bec
shikharkhare13 <shikhar.khare_cs18@gla.ac.in>
2019-08-24 22:28:15 +0530

    Project completed.

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -12,6 +12,10 @@ public class Gravity {
 	public static void main(String[] args) {
 	double distance = 0,time=0,speed=0,g=9.8;
 	Scanner sc = new Scanner(System.in);
+	time=sc.nextDouble();
+	speed=g*time;
+	distance=0.5*g*time*time;
+        System.out.println("The speed of the object at "+time+" seconds after its release is "+ speed+ " and the distance the object has travelled in the " + time + " seconds after the relase is " + distance);
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
