4cb998e9c119f29848cdbfa30a25795b3d82c72a
shikharkhare13 <shikhar.khare_cs18@gla.ac.in>
2019-08-24 22:30:53 +0530

    Project completed!!

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -16,6 +16,7 @@ public class Gravity {
 	speed=g*time;
 	distance=0.5*g*time*time;
         System.out.println("The speed of the object at "+time+" seconds after its release is "+ speed+ " and the distance the object has travelled in the " + time + " seconds after the relase is " + distance);
+	sc.close();
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
