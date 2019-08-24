5bdb02f84b2fc76186352ff375ee9f76eb015492
shikharkhare13 <shikhar.khare_cs18@gla.ac.in>
2019-08-24 22:08:28 +0530

    inital commit

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -1,20 +1,16 @@
 /*
- * @author Divyansh Bhardwaj
+ * @author Shikhar Khare
  * @version 1.0
  * */
 package main;
 
-// import java.util.Scanner;
+import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
-		/* TODO 2:
-		 *   a. Declare all your necessary variables inside main method.
-		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
-		 *   c. Calculate the result and print it to the screen.
-		 * */
+
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
